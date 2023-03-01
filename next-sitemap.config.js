1e9d1e55f3dda2ebfabaaf6ae9b34ac6e418b83d
Jackson Dolman <mail@dolmios.com>
2023-03-01 17:28:34 -0500

    chore: bump design system

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  changefreq: "weekly",
   generateIndexSitemap: false,
   generateRobotsTxt: true,
-  priority: 0.7,
   siteUrl: "https://ihostme.com.au",
   sitemapBaseFileName: "sitemap",
-  sitemapSize: 15000,
 };
