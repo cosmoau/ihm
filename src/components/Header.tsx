3604a61178e72520ea0eb6962414d932546f4ae5
Jackson Dolman <mail@dolmios.com>
2023-09-14 14:57:29 -0400

    refactor: make header layout match brand

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,17 @@
-import { View, Image, Stack, Button, Select, Avatar, type ISelect, useTheme } from "@cosmoau/ui";
-import { CaretDown, GridFour } from "@phosphor-icons/react";
+import {
+  View,
+  Image,
+  Text,
+  Stack,
+  Button,
+  Select,
+  Avatar,
+  type ISelect,
+  useTheme,
+  Menu,
+  useMountSSR,
+} from "@cosmoau/ui";
+import { CaretDown, EnvelopeSimple, GridFour, MapPin } from "@phosphor-icons/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -10,6 +22,7 @@ import logoDark from "../../public/images/logo-new.png";
 
 export default function Header(): JSX.Element {
   const router = useRouter();
+  const mounted = useMountSSR();
   const [inverted, setInverted] = useState(true);
   const { isDarkTheme } = useTheme();
 
@@ -46,7 +59,7 @@ export default function Header(): JSX.Element {
       label: "Sign Up",
       value: "/start",
     },
-  ] as ISelect["options"];
+  ];
 
   const optionsLocations = [
     {
@@ -82,12 +95,14 @@ export default function Header(): JSX.Element {
   };
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
+    if (mounted) {
       window.addEventListener("scroll", () => {
         setInverted(window.scrollY < 500);
       });
     }
-  }, []);
+  }, [mounted]);
+
+  const activeSelection = optionsPhone.find((option) => option.value === router.pathname)?.value || "";
 
   return (
     <header
@@ -98,14 +113,10 @@ export default function Header(): JSX.Element {
         zIndex: 100,
       }}>
       <View bottom="small" inverted={inverted} top="small">
-        <Stack direction="row" flex="center">
-          <Stack direction="column" width={25} widthPhone={50} widthTablet={40}>
-            <Link href="/">
-              <Stack
-                css={{
-                  display: "inline-flex",
-                  verticalAlign: "middle",
-                }}>
+        <Stack css={{ visible: "tablet" }}>
+          <Stack direction="row" flex="center">
+            <Stack direction="column" width={70} widthPhone={70}>
+              <Link href="/">
                 <Image
                   alt="
               ihostme logo, which is a pink icon of a house with black sans-serif text next to it.
@@ -113,84 +124,89 @@ export default function Header(): JSX.Element {
                   height={36.5}
                   src={!inverted && !isDarkTheme ? logoDark.src : logoLight.src}
                   width={120}
-                />
-              </Stack>
-            </Link>
-          </Stack>
-          <Stack
-            align="right"
-            css={{
-              visible: "tablet",
-            }}
-            direction="column"
-            widthPhone={50}
-            widthTablet={60}>
-            <a href="https://v1.cosmogroup.io" rel="noreferrer" target="_blank">
-              <Button inline="small">Clients</Button>
-            </a>
-            <Select
-              horizontal="right"
-              last
-              options={optionsPhone}
-              selection={[optionsPhone.find((option) => option.value === router.pathname)?.value || "/"]}
-              trigger={<Button theme="solid">Menu</Button>}
-              width="16rem"
-              onSelection={(value): void => handleSelection(value)}
-            />
-          </Stack>
-          <Stack
-            align="center"
-            css={{
-              hidden: "tablet",
-            }}
-            direction="column"
-            width={50}>
-            <Link href="/">
-              <Button theme={router.pathname === "/" ? "fill" : "minimal"}>Home</Button>
-            </Link>
-            <Link href="/pricing">
-              <Button theme={router.pathname === "/pricing" ? "fill" : "minimal"}>Pricing</Button>
-            </Link>
-            <Link href="/testimonials">
-              <Button theme={router.pathname === "/testimonials" ? "fill" : "minimal"}>Testimonials</Button>
-            </Link>
-            <Select
-              options={optionsLocations}
-              selection={[router.asPath]}
-              trigger={
-                <Button
-                  icon={<CaretDown weight="light" />}
-                  iconPosition="right"
-                  theme={router.pathname === "/locations" || router.pathname.includes("/city/") ? "fill" : "minimal"}>
-                  Locations
-                </Button>
-              }
-              width="25rem"
-              onSelection={(value): void => handleSelection(value)}
-            />
-            <Link href="/contact">
-              <Button theme={router.pathname === "/contact" ? "fill" : "minimal"}>Contact</Button>
-            </Link>
-            <a href="https://cosmogroup.io/blog" rel="noreferrer" target="_blank">
-              <Button theme="minimal">Blog</Button>
-            </a>
-          </Stack>
+                />{" "}
+              </Link>
+            </Stack>
+            <Stack align="right" direction="column" width={30} widthPhone={30}>
+              <Menu
+                key={router?.pathname}
+                options={optionsPhone}
+                selection={activeSelection}
+                trigger={<Button theme="fill">Menu</Button>}
+                onSelection={(value): void => handleSelection(value)}>
+                <Stack bottom="small">
+                  <Link href="/contact">
+                    <Text accent as="small" inline="auto" link="minimal">
+                      <EnvelopeSimple />
+                      &nbsp; ihostme@cosmogroup.io
+                    </Text>
+                  </Link>
+                </Stack>
 
-          <Stack
-            align="right"
-            css={{
-              hidden: "tablet",
-            }}
-            direction="column"
-            width={25}>
-            <a href="https://v1.cosmogroup.io" rel="noreferrer" target="_blank">
-              <Button inline="small">Client App</Button>
-            </a>
-            <Link href="/start">
-              <Button inline="auto" theme="solid">
-                Sign Up
-              </Button>
-            </Link>
+                <Link href="/contact">
+                  <Text accent as="small" inline="auto" link="minimal">
+                    <MapPin />
+                    &nbsp; 8/100 Dover Street, Cremorne, VIC
+                  </Text>
+                </Link>
+              </Menu>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Stack css={{ hidden: "tablet" }}>
+          <Stack direction="row" flex="center">
+            <Stack direction="column" width={80}>
+              <Stack
+                css={{
+                  display: "inline-flex",
+                  verticalAlign: "middle",
+                  paddingRight: "$medium",
+                }}>
+                <Link href="/">
+                  <Image
+                    alt="
+              ihostme logo, which is a pink icon of a house with black sans-serif text next to it.
+              "
+                    height={36.5}
+                    src={!inverted && !isDarkTheme ? logoDark.src : logoLight.src}
+                    width={120}
+                  />{" "}
+                </Link>
+              </Stack>
+              <Link href="/pricing">
+                <Button theme={router.pathname === "/pricing" ? "fill" : "minimal"}>Pricing</Button>
+              </Link>
+              <Link href="/testimonials">
+                <Button theme={router.pathname === "/testimonials" ? "fill" : "minimal"}>Testimonials</Button>
+              </Link>
+              <Select
+                options={optionsLocations}
+                selection={[router.asPath]}
+                trigger={
+                  <Button
+                    icon={<CaretDown weight="light" />}
+                    iconPosition="right"
+                    theme={router.pathname === "/locations" || router.pathname.includes("/city/") ? "fill" : "minimal"}>
+                    Locations
+                  </Button>
+                }
+                width="25rem"
+                onSelection={(value): void => handleSelection(value)}
+              />
+              <Link href="/contact">
+                <Button theme={router.pathname === "/contact" ? "fill" : "minimal"}>Contact</Button>
+              </Link>
+              <a href="https://cosmogroup.io/blog" rel="noreferrer" target="_blank">
+                <Button theme="minimal">Blog</Button>
+              </a>
+            </Stack>
+            <Stack align="right" direction="column" width={20}>
+              <Link href="/start">
+                <Button inline="auto" theme="fill">
+                  Sign Up
+                </Button>
+              </Link>
+            </Stack>
           </Stack>
         </Stack>
       </View>
